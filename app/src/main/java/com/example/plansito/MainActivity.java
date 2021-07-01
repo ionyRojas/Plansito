c334013fef57570160c0b594bdd9fa19085384f9
javiersandes <san20041@byui.edu>
2021-06-30 23:39:27 -0300

    Create task view on main activity

--- app/src/main/java/com/example/plansito/MainActivity.java
+++ app/src/main/java/com/example/plansito/MainActivity.java
@@ -3,13 +3,25 @@ package com.example.plansito;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tv;
+    private ListView listViewTask;
+
+
     DataSource dataSource;
 
     @Override
@@ -21,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
         dataSource = new DataSource(this);
         dataSource.open();
+        listViewTask = findViewById(R.id.listViewTask);
+        this.getAllTask();
     }
 
     @Override
@@ -33,10 +47,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         dataSource.open();
+        this.getAllTask();
+    }
+
+    private void getAllTask() {
+        SQLiteDatabase db = DataSource.dBConnection.getWritableDatabase();
+        Cursor row = db.rawQuery
+                ("select * from tasks ", null);
+        if (!row.moveToFirst()) {
+            Toast.makeText(this, "Error: yo don't have any tasks", Toast.LENGTH_LONG).show();
+        } else { row.moveToPrevious(); }
+
+        List<String> taskList = new ArrayList<String>();
+
+        while  (row.moveToNext()) {
+            taskList.add(row.getString(0) + " " + row.getString(1) + " " + row.getString(2) + " " + row.getString(3));
+
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                taskList
+        );
+        listViewTask.setAdapter(arrayAdapter);
     }
 
     public void goToCreateTask(View view) {
         Intent intent = new Intent(this, CreateTask.class);
         startActivity(intent);
     }
+
+
 }
